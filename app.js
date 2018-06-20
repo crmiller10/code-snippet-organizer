634f241928d2776f3199dd9ed4a910d327391b8c
Collin Miller <crmiller10@gmail.com>
2018-06-19 22:22:29 -0600

    Updated package.json

--- app.js
+++ app.js
@@ -59,6 +59,32 @@ app.use(
   })
 );
 
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+// if (app.get('env') === 'development') {
+//     app.use((err, req, res, next) => {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err,
+//             title: 'error'
+//         });
+//     });
+// }
+
+// production error handler
+// no stacktraces leaked to user
+// app.use((err, req, res, next) => {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: {},
+//         title: 'error'
+//     });
+// });
 //static files
 app.use(express.static('public/'));
 
